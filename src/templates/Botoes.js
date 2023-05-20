e9f7b18415e97badfcf310df950af0ee05d159ca
Vinicius Alves <moroni.vinicius@hotmail.com>
2023-05-20 02:29:35 -0300

    Alteração nos botões e roteamento dinamico no relatorio do aprendiz

---
+++ src/templates/Botoes.js
@@ -0,0 +1,76 @@
+import { BiArrowBack } from "react-icons/bi";
+import { GrClose } from "react-icons/gr";
+import { Button } from "react-bootstrap";
+import {
+  AiOutlineSave,
+  AiOutlineFileAdd,
+  AiOutlineEdit,
+  AiOutlineDelete,
+  AiOutlineFileText,
+} from "react-icons/ai";
+
+function BotaoVoltar(props) {
+  return (
+    <Button className="mx-1" variant="outline-secondary">
+      <BiArrowBack size={20} /> Voltar
+    </Button>
+  );
+}
+
+function BotaoDetalhes(props) {
+  return (
+    <Button className="mx-1" variant="outline-secondary">
+      <AiOutlineFileText size={20} /> Detalhes
+    </Button>
+  );
+}
+
+function BotaoCancelar(props) {
+  return (
+    <Button className="mx-1" variant="outline-secondary">
+      <GrClose size={20} /> Cancelar
+    </Button>
+  );
+}
+
+function BotaoSalvar(props) {
+  return (
+    <Button className="mx-1" variant="outline-secondary">
+      <AiOutlineSave size={20} /> Salvar
+    </Button>
+  );
+}
+
+function BotaoExcluir(props) {
+  return (
+    <Button className="mx-1" variant="outline-secondary">
+      <AiOutlineDelete size={20} /> Excluir
+    </Button>
+  );
+}
+
+function BotaoEditar(props) {
+  return (
+    <Button className="mx-1" variant="outline-secondary">
+      <AiOutlineEdit size={20} /> Editar
+    </Button>
+  );
+}
+
+function BotaoNovo(props) {
+  return (
+    <Button className="mx-1" variant="outline-secondary">
+      <AiOutlineFileAdd size={20} /> Novo
+    </Button>
+  );
+}
+
+export {
+  BotaoVoltar,
+  BotaoCancelar,
+  BotaoSalvar,
+  BotaoExcluir,
+  BotaoEditar,
+  BotaoNovo,
+  BotaoDetalhes,
+};
--- src/templates/MenuTabela.js
+++ src/templates/MenuTabela.js
@@ -1,37 +1,14 @@
-import { BiArrowBack } from "react-icons/bi";
-import { Button } from "react-bootstrap";
-import {
-  AiOutlineFileAdd,
-  AiOutlineEdit,
-  AiOutlineDelete,
-} from "react-icons/ai";
+// Novo, Detalhes, Excluir
+// Remover botão voltar
+import { BotaoNovo, BotaoExcluir, BotaoDetalhes } from "./Botoes";
 
 export default function MenuTabela(props) {
+
   return (
-    <>
-      <div
-        className="d-flex justify-content-between w-100 mb-3"
-        style={{ height: "40px" }}
-      >
-        <Button variant="light">
-          <BiArrowBack size={20} /> Voltar
-        </Button>
-        <div className="d-flex">
-          <Button
-            variant="light"
-            className="mx-2"
-            onClick={props.chamarCadastro}
-          >
-            <AiOutlineFileAdd size={20} /> Novo
-          </Button>
-          <Button variant="light" className="mx-2" disabled>
-            <AiOutlineEdit size={20} /> Editar
-          </Button>
-          <Button variant="light" disabled>
-            <AiOutlineDelete size={20} /> Excluir
-          </Button>
-        </div>
-      </div>
-    </>
+    <div className="d-flex mb-3">
+      <BotaoNovo />
+      <BotaoDetalhes />
+      <BotaoExcluir />
+    </div>
   );
 }
